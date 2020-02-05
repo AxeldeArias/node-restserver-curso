f219e63182c0dae2df1a55fd765a48f0ffa8f09e
axel dearias <axeldearias@gmail.com>
2020-02-04 22:57:17 -0300

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -11,10 +11,10 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
 
 let urlDB;
-// if (process.env.NODE_ENV === 'dev') {
-//     urlDB = 'mongodb://localhost:27017/cafe'
-// } else {
-urlDB = 'mongodb+srv://axeldearias:kT6nNlUVwGa3QsLD@cluster0-axuwp.mongodb.net/cafe'
-    // }
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe'
+} else {
+    urlDB = process.env.MONGO_URI = urlDB
+}
 
 process.env.URLDB = urlDB
